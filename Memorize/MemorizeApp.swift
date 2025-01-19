fedbcf33fc990f252da7cc2052ed1dfcf9def996
Ulric Marques <ulricmf@hotmail.com>
2025-01-18 22:35:11 -0300

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Ulric Marques on 18/01/25.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
